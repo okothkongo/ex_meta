b365bbb253a069d08a5a169b43c7ab7115440cb5
okoth kongo <okothkongo@gmail.com>
2025-11-02 19:10:53 +0300

    initial commit

---
+++ lib/ex_meta.ex
@@ -0,0 +1,18 @@
+defmodule ExMeta do
+  @moduledoc """
+  Documentation for `ExMeta`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExMeta.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_meta_test.exs
@@ -0,0 +1,8 @@
+defmodule ExMetaTest do
+  use ExUnit.Case
+  doctest ExMeta
+
+  test "greets the world" do
+    assert ExMeta.hello() == :world
+  end
+end
